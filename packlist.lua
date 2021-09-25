bce3ed7782e675da991883001835b1dac8b6d16b
JustDoesGames <42855450+JustDoesGames@users.noreply.github.com>
2021-09-25 11:16:46 -0500

    Create packlist.lua

---
+++ packlist.lua
@@ -0,0 +1,43 @@
+{
+		name = "Mekanism",
+		view = {
+			{
+				name = "Mekanism",
+				link = "mekanism/mekanism",
+				fs_name = "mekanism"
+			},
+			{
+				name = "Mekanism: Generators",
+				link = "mekanism/mekanismgenerators",
+				fs_name = "mekanismgenerators"
+			}
+		},
+	},
+	{
+		name = "Minecraft",
+		view = {
+			{
+				name = "Minecraft 1.12",
+				link = "minecraft/1-12",
+				fs_name = "minecraft_1-12"
+			}
+		}
+	},
+	{
+		name = "Computercraft",
+		view = {
+			{
+				name = "Computercraft: Tweaked",
+				link = "computercraft/cc-tweaked",
+				fs_name = "cc-tweaked"
+			}
+		},
+  {
+    name = "Iron Chest",
+    view = {
+      name = "Iron Chest",
+      link = "ironchest/ironchest",
+      fs_name = "ironchest"
+    }
+  }
+	},
